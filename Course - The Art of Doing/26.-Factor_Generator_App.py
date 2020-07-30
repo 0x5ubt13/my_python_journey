40fa7a37c744041b69f582d4004a37f28a61fd04
0x5ubt13 <5ubt13@protonmail.com>
2020-07-30 16:59:01 +0100

    committing new program

---
+++ Course - The Art of Doing/26.-Factor_Generator_App.py
@@ -0,0 +1,22 @@
+#/usr/bin python3
+
+print("Welcome to the Factor Generator App")
+while True:
+    i = 1
+    factors = []
+    key_number = int(input("\nEnter a number to determine all factors of it: "))
+    print("Factors of " + str(key_number) + " are:")
+    while i <= key_number:
+        if key_number % i == 0:
+            print(i)
+            factors.append(i)
+        i += 1
+    print("\nIn summary:")
+    for i in range((len(factors)+1)//2):
+        if i == 0:
+            print(factors[i], "*", factors[-1], '=', str(key_number))
+        else:
+            print(factors[i], "*", factors[-i-1], '=', str(key_number))
+    choice = input("Run again? (y/n) ")
+    if choice == 'n':
+        break
