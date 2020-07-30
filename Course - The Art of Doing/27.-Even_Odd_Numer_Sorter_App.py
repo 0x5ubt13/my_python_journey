8c5728483865e3d1834d94b59f9529a556c2074a
0x5ubt13 <5ubt13@protonmail.com>
2020-07-30 18:06:32 +0100

    uploading 27

--- Course - The Art of Doing/27.-Even_Odd_Numer_Sorter_App.py
+++ Course - The Art of Doing/27.-Even_Odd_Numer_Sorter_App.py
@@ -6,23 +6,25 @@ even = []
 print('Welcome to the Even Odd Numer Sorter App.')
 
 while True:
-    choice = list(input("Please enter your numbers: ").replace(",", " ").strip().split())
+    choice = input("Please enter your numbers: ").replace(",", " ").strip().split()
     print("---- Result Summary ----")
     for i in choice:
         if int(i) % 2 == 0:
-            print(str(i), "is even!")
+            print('\t', i, "is even!")
             even.append(i)
         else:
-            print(i, "is odd!")
+            print('\t', i, "is odd!")
             odd.append(i)
 
+    even.sort()
     print("The following " + str(len(even)) + " numbers are even: ")
     for i in even:
         print(i)
 
+    odd.sort()
     print("The following " + str(len(odd)) + " numbers are odd: ")
     for i in odd:
         print(i)
-    breaking = input("Would you like to run the program again? (y/n) ")
+    breaking = input("Would you like to run the program again? (y/n) ").lower()
     if breaking == 'n':
         break
