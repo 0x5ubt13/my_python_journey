b7ee584d39c59f7620f32a38fa98b870b7bc6a1d
0x5ubt13 <5ubt13@protonmail.com>
2020-08-20 16:02:43 +0100

    advancing in #36

--- Course - The Art of Doing/36.-Pythonagachi_Simulator_app.py
+++ Course - The Art of Doing/36.-Pythonagachi_Simulator_app.py
@@ -4,16 +4,17 @@ import random
 class Tamagotchi():
     """ Class to simulate a tamagotchi """
     
-    def __init__(self, name, hunger, boredom, tiredness, dirtiness, food_inventory, awake):
+    def __init__(self, name, difficulty=3):
         """ Initializing attributes """
-        self.name = name.title()
+        self.name = name.title() 
+        self.difficulty = 0
+        
         self.hunger = 0
         self.boredom = 0
         self.tiredness = 0
         self.dirtiness = 0
         self.food_inventory = 0
-        self.is_awake = awake #boolean
-
+        self.is_awake = True
         self.is_alive = True
         self.turns_alive = 0
 
@@ -21,10 +22,13 @@ class Tamagotchi():
         """ Display info on the tamagotchi """
         print("Hunger (1-10):\t" + str(self.hunger))
         print("Boredom (1-10):\t" + str(self.boredom))
-        print("Tiredness (1-10):\t" + str(self.year_built))
-        print("Dirtiness (1-10):\t" + str(self.price))
+        print("Tiredness (1-10):\t" + str(self.tiredness))
+        print("Dirtiness (1-10):\t" + str(self.dirtiness))
         print("\nFood Inventory " + str(self.food_inventory) + " pieces")
-        print("Current status: " + str(self.awake))
+        if self.is_awake:
+            print("Current status: Awake")
+        else:
+            print('Current status: Sleeping')
         print("""
                        Menu:
             ~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -36,14 +40,18 @@ class Tamagotchi():
             ~~~~~~~~~~~~~~~~~~~~~~~~~~
         """)
 
+
     def summary(self):
         """ Display info on the tamagotchi after the round"""
         print("Hunger (1-10):\t" + str(self.hunger))
         print("Boredom (1-10):\t" + str(self.boredom))
-        print("Tiredness (1-10):\t" + str(self.year_built))
-        print("Dirtiness (1-10):\t" + str(self.price))
+        print("Tiredness (1-10):\t" + str(self.tiredness))
+        print("Dirtiness (1-10):\t" + str(self.dirtiness))
         print("\nFood Inventory " + str(self.food_inventory) + " pieces")
-        print("Current status: " + str(self.awake))
+        if self.is_awake:
+            print("Current status: Awake")
+        else:
+            print('Current status: Sleeping')
         input("\n\nPress (enter) to continue...")
 
     def eat(self):
@@ -76,15 +84,47 @@ class Tamagotchi():
     
     def food(self):
         """ make the tamagotchi go out for food """
-        
+        food_inventory += random.randint(1,3)
+
+
     def alive(self):
         """ checks whether the tamagotchi is alive or not """
+        if self.hunger == 10 or self.boredom == 10 or self.tiredness == 10 or self.dirtiness == 10 or self.food_inventory == 10:
+            self.is_alive = False
+            if self.hunger == 10:
+                print(f'{self.name} has starved')
 
     def turn(self):
         """ loops through a new turn """
         self.turns_alive += 1
-        self.hunger += random.randint(0, 3)
-        self.boredom += random.randint(0, 3)
-        self.tiredness += random.randint(0, 3)
-        self.dirtiness += random.randint(0, 3)
+        self.hunger += random.randint(0, difficulty)
+        self.boredom += random.randint(0, difficulty)
+        self.tiredness += random.randint(0, difficulty)
+        self.dirtiness += random.randint(0, difficulty)
+        self.is_alive()
+        if self.is_alive == False:
+            print(f"Well done, your pet {self.name} survived for {self.turns_alive} turns")
+            alive == False
+        else:
+            choice = int(input("What's your choice? "))
+            if 
+        
+        
+alive = True
+while alive:
+    difficulty = int(input("Please enter the desired level of difficulty (1-5):" ))
+    name = input("What's your new pet's name? ")
+    pet = Tamagotchi(name, difficulty)
+    pet.display_info()
+    turn()
+    summary()
+
+
+
+
+
+
+
+
+    
         
