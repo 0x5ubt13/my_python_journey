8802998eb821653bb8605623419473249c43824d
0x5ubt13 <5ubt13@protonmail.com>
2020-08-22 23:28:28 +0100

    working on #37

--- Course - The Art of Doing/37.-Casino_Blackjack_app.py
+++ Course - The Art of Doing/37.-Casino_Blackjack_app.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
 import random
+import time
 
 class Game():
     """ Coding the game behaviour """
 
     def __init__(self, money=100):
+        """ Initialising the Class """
+        
         self.money = money
         
         self.bet = 0
+        # List to get the cards off the pool once drew
         self.used_cards = []
+        # We need to define all 53 cards used in Blackjack
         self.deck = {
             "Hearts":["Ace of Hearts", "2 of Hearts", "3 of Hearts", "4 of Hearts", "5 of Hearts", "6 of Hearts", "7 of Hearts", "8 of Hearts", "9 of Hearts", "10 of Hearts", "J of Hearts", "Q of Hearts", "K of Hearts"], 
             "Spades":["Ace of Spades", "2 of Spades", "3 of Spades", "4 of Spades", "5 of Spades", "6 of Spades", "7 of Spades", "8 of Spades", "9 of Spades", "10 of Spades", "J of Spades", "Q of Spades", "K of Spades"],
@@ -20,15 +25,26 @@ class Game():
         self.players_score = 0
         self.houses_hand = []
         self.houses_score = 0
+        self.turn = 0
 
+    
     def draw(self):
-        while True:    
+        """ We need a method for randomly drawing a card, erase it from the pool and assigning it a value """
+        
+        while True:
+            # Randomising keys and values from the Deck dictionary    
             randomising_suit = random.randint(0, 3)
             randomising_card = random.randint(0, 12)
-            randsuit = list(self.deck.keys())[randomising_suit] # In order to index the key in python3, we need to parse .keys() to a list since the method returns an iterable but not indexable object!!!
+
+            # In order to index the key in python3, we need to parse .keys() to a list since the method returns an iterable but not indexable object!!!
+            randsuit = list(self.deck.keys())[randomising_suit] 
             randcard = self.deck[randsuit][randomising_card]
+            
+            # Checking if the card is already been drawn
             if randcard in self.used_cards:
                 continue
+
+            # Assigning initial values to the cards
             if randcard.startswith("A"):
                 self.values += 11
             elif randcard.startswith("2"):
@@ -47,52 +63,116 @@ class Game():
                 self.values += 8
             elif randcard.startswith("9"):
                 self.values += 9
-            elif randcard.startswith("10") or randcard.startswith("J") or randcard.startswith("J") or randcard.startswith("K")
+            elif randcard.startswith("10") or randcard.startswith("J") or randcard.startswith("Q") or randcard.startswith("K"):
                 self.values += 10
-            
+
+            # Appending it to the list of used cards below takes them off of the drawing pool
             self.used_cards.append(randcard)
             break
         return(randcard)
     
     def betting(self):
+        """ Method to set the bet and show the first card of the dealer """
+        
         print(f"Current money: £{self.money}")
         self.bet = int(input("What would you like to bet (minimum bet of £20): "))
+        
+        # Since the minimum bet is £20, we need a little control flow to meet this criteria
         if self.bet < 20:
             self.bet = 20
+        
+        self.money -= self.bet
+
+        # Printing a summary for the player and showing the first dealer's card    
         print(f"Current money: £{self.money}  |  Current bet: £{self.bet}")
         print(f"\nThe dealer is showing a {self.houses_hand[0]}")
 
 
-    def house_turn(self):
-        print("house's turn:")
-        self.houses_hand = self.draw()
-        print(self.houses_hand)
 
+    def house_turn(self):
+        """ Method to make the house play """
+
+        self.turn += 1
+        print(f"house's turn: {self.turn}") # debugging, borrame
+        if self.turn == 1:
+            self.houses_hand.append(self.draw())
+            self.houses_score += self.values
+            self.values = 0
+            print(self.houses_hand) # debugging, borrame
+        elif self.turn == 2:
+            self.houses_hand.append(self.draw())
+            self.houses_score += self.values
+            self.values = 0
+            while self.houses_score >= 16:                
+                self.houses_hand.append(self.draw())
+                self.houses_score += self.values
+                self.values = 0
 
 
     def player_turn(self):
-        print("player's turn:")
-        self.players_hand = self.draw()
+        """ Method to make the player play """
 
+        print("player's turn:") # debugging, borrame
+        for i in range(2):
+            self.players_hand.append(self.draw()) 
+            self.players_score += self.values
+            self.values = 0
+        for card in self.players_hand:
+            print(card)
+        print("Total value: ", self.players_score)
+        
+        not_score_enough = True
+        while not_score_enough:
+            hit = input("Do you want to hit? (y/n) ").lower().strip()
+            if hit == "y":
+                print("hit y")
+                self.players_hand.append(self.draw()) 
+                self.players_score += self.values
+                self.values = 0
+                for card in self.players_hand:
+                    print(card)
+                print("Total value: ", self.players_score)
+                
+                if self.players_score >= 22:
+                    not_score_enough = False
+
+            else:
+                not_score_enough = False
+
+
+    def reveal_houses_hand(self):
+        """ Reveal the dealer's hand and and finishes the game """
+        
+        if self.players_score >= 22:
+            print("You went over 21... You loose!")
+        print("\nDealer is set with a total of", len(self.houses_hand), "cards.")
+        input("\nPress (enter) to reveal the dealer's hand.")
+        for card in self.houses_hand:
+            time.sleep(1.5)
+            print(card)
+        if self.players_score >= 22:
+            print("You went over 21... You loose!")
+        else:
+            if self.houses_score >=
+            
+        
 
         
         
  
-playing = True
-while playing:
-    print("Welcome to the Blackjack App.")
-    print("The minimum bet at this table is £20.")
-    game = Game(int(input("How much money are you willing to play with today? £")))
+print("Welcome to the Blackjack App.")
+print("The minimum bet at this table is £20.")
+game = Game(int(input("How much money are you willing to play with today? £")))
 
+while game > 0:
     game.house_turn()
-
     game.betting()
+    game.player_turn()
+    game.house_turn()
+    game.reveal_houses_hand()
 
+print("\nSorry, you ran out of money. Please play again.")
 
-
-    again = input("Play again? (y/n) ").lower().strip()
-    if again == "n":
-        playing = False
 
 
 
