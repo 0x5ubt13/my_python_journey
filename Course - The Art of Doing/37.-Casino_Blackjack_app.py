b85b93bfba692affdbdd7544a4e86c69be205ba9
0x5ubt13 <5ubt13@protonmail.com>
2020-08-23 23:24:53 +0100

    finishing #37

--- Course - The Art of Doing/37.-Casino_Blackjack_app.py
+++ Course - The Art of Doing/37.-Casino_Blackjack_app.py
@@ -75,17 +75,24 @@ class Game():
         """ Method to set the bet and show the first card of the dealer """
         
         print(f"Current money: £{self.money}")
-        self.bet = int(input("What would you like to bet (minimum bet of £20): "))
-        
-        # Since the minimum bet is £20, we need a little control flow to meet this criteria
-        if self.bet < 20:
-            self.bet = 20
-        
+        while True:
+            self.bet = int(input("What would you like to bet (minimum bet of £20): "))
+
+            # Since the minimum bet is £20, we need a little control flow to meet this criteria
+            if self.bet < 20:
+                self.bet = 20
+            
+            
+            if self.bet > self.money:
+                print("You don't have money enough to make that bet.") 
+                continue
+
+            break
         self.money -= self.bet
 
         # Printing a summary for the player and showing the first dealer's card    
         print(f"Current money: £{self.money}  |  Current bet: £{self.bet}")
-        print(f"\nThe dealer is showing a {self.houses_hand[0]}")
+        print(f"\nThe dealer is showing a {self.houses_hand[0]}\n")
 
 
 
@@ -93,17 +100,15 @@ class Game():
         """ Method to make the house play """
 
         self.turn += 1
-        print(f"house's turn: {self.turn}") # debugging, borrame
         if self.turn == 1:
             self.houses_hand.append(self.draw())
             self.houses_score += self.values
             self.values = 0
-            print(self.houses_hand) # debugging, borrame
         elif self.turn == 2:
             self.houses_hand.append(self.draw())
             self.houses_score += self.values
             self.values = 0
-            while self.houses_score >= 16:                
+            while self.houses_score <= 16:                
                 self.houses_hand.append(self.draw())
                 self.houses_score += self.values
                 self.values = 0
@@ -112,7 +117,7 @@ class Game():
     def player_turn(self):
         """ Method to make the player play """
 
-        print("player's turn:") # debugging, borrame
+        print("Your cards:")
         for i in range(2):
             self.players_hand.append(self.draw()) 
             self.players_score += self.values
@@ -121,8 +126,7 @@ class Game():
             print(card)
         print("Total value: ", self.players_score)
         
-        not_score_enough = True
-        while not_score_enough:
+        while True:
             hit = input("Do you want to hit? (y/n) ").lower().strip()
             if hit == "y":
                 print("hit y")
@@ -132,44 +136,76 @@ class Game():
                 for card in self.players_hand:
                     print(card)
                 print("Total value: ", self.players_score)
-                
+    
                 if self.players_score >= 22:
-                    not_score_enough = False
-
+                    break
             else:
-                not_score_enough = False
+                break
+        if len(self.players_hand) == 2 and "A" in self.players_hand and self.players_score == 21:
+            print("Congratulations, you've scored a Blackjack!!!!")
 
 
     def reveal_houses_hand(self):
         """ Reveal the dealer's hand and and finishes the game """
         
-        if self.players_score >= 22:
-            print("You went over 21... You loose!")
         print("\nDealer is set with a total of", len(self.houses_hand), "cards.")
         input("\nPress (enter) to reveal the dealer's hand.")
         for card in self.houses_hand:
-            time.sleep(1.5)
+            time.sleep(0.5)
             print(card)
         if self.players_score >= 22:
             print("You went over 21... You loose!")
         else:
-            if self.houses_score >=
-            
-        
+            if self.players_score == self.houses_score:
+                print(f"Dealer gets {self.houses_score}.")
+                print("It's a tie!!")
+                self.money += self.bet
+            else:
+                if self.houses_score >= 22:
+                    print("Dealer went over 21... You win!")
+                    self.money += self.bet * 2
+                else:
+                    if len(self.players_hand) == 2 and "A" in self.players_hand and self.players_score == 21:
+                        print("You've scored a Blackjack!!!!")
+                        self.money += self.bet * 2
+                    else:
+                        if self.players_score > self.houses_score:
+                            print(f"Dealer gets {self.houses_score}.")
+                            print("You win!!")
+                            self.money += self.bet * 2
+                        else:
+                            print(f"Dealer gets {self.houses_score}.")
+                            print("The House wins!")
 
-        
-        
+    def wipe(self):
+        """ Wipes the game out when a round is finished """
+
+        # Re-declaring variables
+        self.values = 0
+        self.players_hand = []
+        self.players_score = 0
+        self.houses_hand = []
+        self.houses_score = 0
+        self.turn = 0
+
+        # Checking the deck
+        if len(self.used_cards) > 45:
+            print("There are only", 52 - len(self.used_cards), "cards to play with. Shuffling...")
+            self.used_cards = []
+
+
  
 print("Welcome to the Blackjack App.")
 print("The minimum bet at this table is £20.")
 game = Game(int(input("How much money are you willing to play with today? £")))
 
-while game > 0:
+while game.money > 0:
     game.house_turn()
     game.betting()
     game.player_turn()
     game.house_turn()
     game.reveal_houses_hand()
+    game.wipe()
 
 print("\nSorry, you ran out of money. Please play again.")
 
