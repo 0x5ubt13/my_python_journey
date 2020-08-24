3a5fe41eb3f85e9abaf3f574d2ae7b7eba7e9dee
0x5ubt13 <5ubt13@protonmail.com>
2020-08-24 16:54:27 +0100

    starting #38

---
+++ Course - The Art of Doing/38.-Pykemon_Simulator_App.py
@@ -0,0 +1,56 @@
+#!/bin/env/python3
+
+class Pykemon():
+    """ Class to define the Pykemons """
+
+    def __init__(self):
+
+        self.health = 0
+        self.element_types = ["FIRE", "WATER", "GRASS"]
+        self.speed = 0
+        self.attacks = []
+        self.
+
+
+
+
+
+
+
+
+
+
+
+print("""
+Welcome to Pykemon!
+Can you become the worlds greatest Pykemon Trainer???
+
+Don't worry! Prof 5ubt13 is here to help you on your quest.
+He would like to gift you your first Pykemon!
+Here are three potential Pykemon partners.
+""")
+
+input("Press (enter) to choose your Pykemon!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
