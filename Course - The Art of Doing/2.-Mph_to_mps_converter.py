1934b3eff8d8011d900305e12c4884275bc1f4e1
0x5ubt13 <5ubt13@protonmail.com>
2024-11-25 23:31:20 +0000

    Chore - compiling all individual python files into a single jupyter notebook to tidy up the GitHub repo

--- Course - The Art of Doing/2.-Mph_to_mps_converter.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-print("Welcome to 5ubt13's MPH to MPS conversion program!")
-
-MPH = float(input("\n Please enter here the MPH you want to convert: "))
-
-MPS = round(MPH / 2.237, 2)
-
-print(MPS)
